06777dc4af848e951dff91f627ef2d4681671a86
yotaro1019 <aloha.yotaro@gmail.com>
2020-04-18 21:28:01 +0900

    Add : create constructor for restart

--- restart.cpp
+++ restart.cpp
@@ -1,18 +1,21 @@
 //restart system
 #include "restart.h"
 
-void Restart::Restart(){
-    Baseout::Baseout(); 
+
+
+Restart::Restart() : Baseout(){
+     
 
 }
 
 void Restart::output_initialize(){
+    this->c_switch = true;
     std::string fname = "restart.out";
     fout.reset(new std::ofstream( fname.c_str() ) );
     check_file_status(fout, fname);
 }
 
 
-void Restart::output_write(double act_force[6]){
+void Restart::output_record_force_momennt(ChVector<> fforce, ChVector<> fmoment){
 
 }
--- restart.h
+++ restart.h
@@ -1,9 +1,20 @@
+#ifndef _vehicle_restart_
+#define __vehicle_restart_
+
 #include"baseout.h"
+#include "chrono/core/ChVector.h"
+#include "chrono/core/ChQuaternion.h"
+
+
+using namespace chrono;
+
 
 class Restart : public Baseout{
   public:
     Restart();
     void output_initialize();
-    void output_write(double act_force[6]);
+    void output_record_force_momennt(ChVector<> fforce, ChVector<> fmoment);
 
 };
+
+#endif
