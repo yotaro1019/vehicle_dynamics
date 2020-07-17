8aefd7401f6d788dfe9d0d609abf77c57821389a
yotaro1019 <aloha.yotaro@gmail.com>
2020-07-18 08:53:17 +0900

    Fix : chenge dir oc output pov geom.inc

--- vehicle_core.cpp
+++ vehicle_core.cpp
@@ -303,15 +303,8 @@ void Vehicle_model::initialize_pov(){
         return;
     }
 
-
-    std::string pov_inc_dir = pov_dir + "/inc";    
-    if (!filesystem::create_directory(filesystem::path(pov_inc_dir))) {
-        std::cout << "Error creating directory " << pov_inc_dir << std::endl;
-        return;
-    }
-
-    terrain->ExportMeshPovray(pov_inc_dir);
-    driver_follower->ExportPathPovray(pov_inc_dir);
+    terrain->ExportMeshPovray(pov_dir);
+    driver_follower->ExportPathPovray(pov_dir);
 
     pov_dir += "/data";   
     if (!filesystem::create_directory(filesystem::path(pov_dir))) {
