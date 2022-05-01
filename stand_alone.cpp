86e732d402fc43611db1d6179839f8ce90cdf35d
yotaro <aloha.yotaro@gmail.com>
2022-05-01 16:47:09 +0900

    standaloneのステップ数をコマンドライン引数で指定

--- stand_alone.cpp
+++ stand_alone.cpp
@@ -2,13 +2,19 @@
 #include"c_vehicle_core.h"
 
 
-int main(){
+int main(int argc, char* argv[]){
+    int end_step = atoi(argv[1]);
+    
+    std::cout << " end_step:" << end_step << "\n";
     vehicle_initialize_stand_alone();
-
+    
+    
+    
     std::cout << "START\n";
-    for(int i=0; i<10000; i++){
+    for(int i=0; i<end_step; i++){
         std::cout << " \n";
         vehicle_advance_stand_alone();
+        
     }
 
 }
